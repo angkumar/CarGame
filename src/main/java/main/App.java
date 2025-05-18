25b8b8f75e2d74dac8dc2fb7b35ea41940a67d77
angkumar <angkumar787@gmail.com>
2025-05-18 19:03:52 -0400

    idk anymore

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -8,7 +8,7 @@ public class App {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Car Games");
+        window.setTitle("Sigma game");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
