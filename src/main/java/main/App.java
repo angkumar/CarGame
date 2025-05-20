5e876051650f16690c929db6db88370d79488657
angkumar <angkumar787@gmail.com>
2025-05-19 20:48:35 -0400

    new stuff

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -8,7 +8,7 @@ public class App {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Sigma game");
+        window.setTitle("Sigma Sigma");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
